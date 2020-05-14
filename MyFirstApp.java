5b1124114d434516ba359f438c040bce851ec279
soeren.grannemann <64766572+SGrannemann@users.noreply.github.com>
2020-05-14 19:53:43 +0200

    Exercises and small code snippets from Chapter 1.

---
+++ MyFirstApp.java
@@ -0,0 +1,12 @@
+public class MyFirstApp{
+  public static void main (String[] args){
+    System.out.println("I Rule!");
+    System.out.println("The World");
+    int x = 15;
+    while (x > 10)  {
+      System.out.println("X before is:" + x);
+      x = x - 1;
+      System.out.println("X after is:" + x);
+    }
+  }
+}
