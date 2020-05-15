7c2145d7f6d4b7b2430a2cbc01548d9da05db2f7
SGrannemann <64766572+SGrannemann@users.noreply.github.com>
2020-05-15 19:07:22 +0200

    Simple coding from Ch2.

---
+++ DogTestDrive.java
@@ -0,0 +1,10 @@
+class DogTestDrive{
+  public static void main (String[] args) {
+    // make a new object
+    Dog d = new Dog();
+    // set instance variable size to 40
+    d.size = 40;
+    // call method bark
+    d.bark();
+  }
+}
