4737ebdb8a85c389d97fc7206b4b6456c003fa79
SGrannemann <64766572+SGrannemann@users.noreply.github.com>
2020-07-07 06:36:18 +0200

    Completed the random quiz generator.

--- Ch9/randomQuizGenerator.py
+++ Ch9/randomQuizGenerator.py
@@ -48,6 +48,13 @@ for quizNum in range(35):
         answerOptions = wrongAnswers + [correctAnswer]
         random.shuffle(answerOptions)
 
-        # TODO: Write the question and answer options to the quiz file.
-
-        # TODO: Write the answer key to a file.
+        # Write the question and answer options to the quiz file.
+        quizFile.write(f'{questionNum + 1}. What is the capital of
+                       {states[questionNum]}?\n')
+        for i in range(4):
+            quizFile.write(f"  {'ABCD'[i]}. {answerOptions[i]}\n")
+        quizFile.write('\n')
+        # Write the answer key to a file.
+        answerKeyFile.write(f"{questionNum + 1}.{'ABCD'[answerOptions.index(correctAnswer)]}")
+        quizFile.close()
+        answerKeyFile.close()
