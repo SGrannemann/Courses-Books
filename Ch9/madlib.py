3bf4ca2d6ef020942464ce6928eb906c00c7c12b
SGrannemann <64766572+SGrannemann@users.noreply.github.com>
2020-07-12 08:16:57 +0200

    finished MadLib practice project.

--- Ch9/madlib.py
+++ Ch9/madlib.py
@@ -9,12 +9,17 @@ split_sentence = sentence.split()
 print(split_sentence)
 # Prompt the user for a word for each occurrence of ADJECTIVE, NOUN, VERB
 # in the text
+[]
 words_to_replace = ['adjective', 'noun', 'verb.']
 for index, word in enumerate(split_sentence):
     if word.lower() in words_to_replace:
+        if word.lower() == 'verb.':
+            userInput = input(f"Enter an {word.lower()}\n")
+            split_sentence[index] = userInput + '.'
         # Replace the word in the list with the user input
-        userInput = input(f"Enter an {word.lower()}.\n")
-        split_sentence[index] = userInput
+        else:
+            userInput = input(f"Enter an {word.lower()}.\n")
+            split_sentence[index] = userInput
 
 # Join the list into a string
 new_sentence = ' '.join(split_sentence)
