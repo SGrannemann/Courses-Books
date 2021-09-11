600656d4e47c4b289d4fe7a8047728cbe1cc7bc0
Soeren Grannemann <64766572+SGrannemann@users.noreply.github.com>
2021-09-11 12:30:56 +0200

    added rest of chapter 3. started working on the MNIST classifier.

---
+++ Hands-on ML/MNIST.py
@@ -0,0 +1,30 @@
+from sklearn.datasets import fetch_openml
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score, GridSearchCV
+mnist = fetch_openml('mnist_784', version=1)
+
+
+# train test split
+
+X, y =mnist['data'], mnist['target']
+y = y.astype(np.uint8)
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+# use kneighbors classifier for baseline
+
+knn_clf = KNeighborsClassifier()
+#knn_clf.fit(X_train, y_train)
+
+#knn_clf.predict(X_test)
+
+params = [{'weights': ['uniform'], 'n_neighbors': [1, 5, 10, 100, 1000]}, {'weights': ['distance'], 'n_neighbors': [1, 5, 10, 100, 1000]}]
+
+# start a grid search
+
+grid_search = GridSearchCV(knn_clf, param_grid=params, scoring='accuracy', cv=3)
+grid_search.fit(X_train, y_train)
+print(grid_search.best_estimator_)
+
+# distance is optimal, n_neighbors doesnt seem to make a difference
+
+# accuracy should be greater than 97 % (evaluate that)
