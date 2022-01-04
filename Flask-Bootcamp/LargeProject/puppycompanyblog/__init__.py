028cad363c98c59f55c32581496eac8fc8dc6804
Soeren Grannemann <64766572+SGrannemann@users.noreply.github.com>
2022-01-04 10:39:13 +0100

    started working on the final large project.

---
+++ Flask-Bootcamp/LargeProject/puppycompanyblog/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+#################################
+#### Database Setup #############
+#################################
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
+    os.path.join(basedir, 'data.sqlote')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+############################
+###### LOGIN Configs ######
+###########################
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+from puppycompanyblog.core.views import core
+from puppycompanyblog.error_pages.handlers import error_pages
+from puppycompanyblog.users.views import users
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
+app.register_blueprint(users)
